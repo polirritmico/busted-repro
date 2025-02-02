479ad2620025ac1882ed10b80f116ebd23e79fa0
Eduardo Bray <ebray187@gmail.com>
2025-02-02 13:54:28 -0300

    test: update busted.lua. Add path set and print statement

--- case/tests/busted.lua
+++ case/tests/busted.lua
@@ -7,3 +7,10 @@ load(vim.fn.system("curl -s https://raw.githubusercontent.com/folke/lazy.nvim/ma
 require("lazy.minit").busted({
   spec = {},
 })
+
+
+-- BUG: The path must be added to run the tests with local-lua-debugger.
+-- As a workaround I've tried this, but it runs only on the non-dap strategy.
+package.path = package.path .. ";" .. vim.uv.cwd() .. "/?.lua"
+print("updated path:")
+print(package.path)
